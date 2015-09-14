15a3cabfae75b2f4d1acf4dfe1c398c968339b95
nickolanack <nickblackwell82@gmail.com>
2015-09-14 05:33:45 -0700

    base file

--- screengrab.js
+++ screengrab.js
@@ -1,3 +1,38 @@
 /**
  * 
  */
+
+console.log(JSON.stringify(phantom.args));
+
+phantom.exit(0);
+var page = require('webpage').create();
+page.open('http://sitename', function(status) {
+  console.log("Status: " + status);
+  if(status === "success") {
+ 
+    var render=function(size){
+        
+	page.viewportSize = size;
+        page.render('page'+size.width+'x'+size.height+'.png');
+
+	page.viewportSize = {width:size.height, height:size.width};
+	page.render('page'+size.height+'x'+size.width+'.png');	
+
+   }
+
+    setTimeout(function(){
+      
+	render({ width: 768, height: 1024 });
+	render({ width: 640, height: 980 });
+
+      phantom.exit();
+
+
+
+   },5000);
+    
+  }else{
+  	phantom.exit();
+  }
+
+});
